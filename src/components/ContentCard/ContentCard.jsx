b589ece11ca24d5e9fb8147ae4594b721ce730be
Razvan Paros <razvanparos21@gmail.com>
2024-03-16 02:22:17 +0200

    desktop version

--- src/components/ContentCard/ContentCard.jsx
+++ src/components/ContentCard/ContentCard.jsx
@@ -1,10 +1,15 @@
+
 import './ContentCard.css'
 
 function ContentCard(props){
     return(
         <div className='content-card-div'>
             <img className='card-img' src={`http://localhost:1337${props.image}`} alt="" />
-            <p className='card-title'>{props.title}</p>
+            <div className='card-bottom'>
+                <p>{`${props.year} - ${props.type==='movie'?'Movie':'TV Series'}`}</p>
+                <p className='card-title'>{props.title}</p>
+            </div>
+           
         </div>
     );
 }
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,66 +3,138 @@ import './Dashboard.css'
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import ContentCard from '../../components/ContentCard/ContentCard';
+import Navbar from '../../components/Navbar/Navbar';
+import Loader from '../../components/Loader/Loader';
 
 function Dashboard(){
     const navigate = useNavigate();
     const [isLoggedIn, setIsLoggedIn]=useState(localStorage.getItem('LoggedIn'));
     const [currentUserId, setCurrentUserId]=useState(localStorage.getItem('currentUserId'));
     const [userData, setUserData]=useState('');
+    const [searchInput, setSearchInput]=useState('');
+    const [searchFor, setSearchFor]=useState('movies or TV series');
     const [contentData, setContentData]=useState([]);
-    const [loading, setLoading]=useState(false);
+    const [originalContentData, setOriginalContentData]=useState([]);
+    const [loadingUserData, setLoadingUserData] = useState(false);
+    const [loadingContentData, setLoadingContentData] = useState(false);
     
     useEffect(() => {
         if (isLoggedIn === "false" || !isLoggedIn) {
             navigate('/login');
         }
     }, [isLoggedIn]);
+    
     useEffect(() => {
-        setLoading(true);
+        setLoadingUserData(true);
         axios.get(`http://localhost:1337/api/users/${currentUserId}`)
             .then(response=>{
                 setUserData(response.data)
-                setLoading(false);
+                setLoadingUserData(false);
             })
             .catch(error => {
                 console.error('Error fetching user data:', error);
             });
     }, []);
     useEffect(() => {
-        axios.get(`http://localhost:1337/api/contents?populate=*&pagination[pageSize]=30`)
+        setLoadingContentData(true);
+        setTimeout(() => {
+             axios.get(`http://localhost:1337/api/contents?populate=*&pagination[pageSize]=30`)
             .then(response=>{
-                setContentData(response.data.data) 
-                
+                setContentData(response.data.data)
+               
+                setLoadingContentData(false);      
             })
             .catch(error => {
                 console.error('Error fetching user data:', error);
             });
+          }, "50");
+       
     }, []);
     useEffect(() => {
        console.log(contentData)
     }, [contentData]);
     
+    useEffect(() => {
+        let originalData=originalContentData;
+        const filteredData = originalData.filter(item =>
+            item.attributes.title.toLowerCase().includes(searchInput.toLowerCase())
+        );
+        setContentData(filteredData);
+    }, [searchInput]);
+
+    const handleChangeFilter=(filter)=>{
+        setSearchInput('')
+        let filterQuery='';
+        switch(filter){
+            case 'movie':
+                setSearchFor('movies')
+                filterQuery=`filters[type][$eq]=${filter}&`;
+                break;
+            case 'series':
+                setSearchFor('TV series')
+                filterQuery=`filters[type][$eq]=${filter}&`;
+                break;
+            case 'all':
+                setSearchFor('movies or TV series')
+                filterQuery=``;
+                break;
+        }
+        setLoadingContentData(true);
+        setTimeout(() => {
+            axios.get(`http://localhost:1337/api/contents?${filterQuery}populate=*&pagination[pageSize]=30`)
+            .then(response=>{
+                setContentData(response.data.data)  
+                setOriginalContentData(response.data.data)
+                setLoadingContentData(false);    
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+            });
+          }, "50");
+        
+       
+    }
+    
     
     return(
         <div className="dashboard-div">
-            {/* <p className={`welcome`}>{`${loading?'loading...':`Welcome, ${userData.username}`}`}</p>
-            <button onClick={()=>{
-                localStorage.setItem('LoggedIn',false)
-                localStorage.setItem('RememberMe',false)
-                localStorage.setItem('currentUserId',0)
-                navigate('/login')
-            }}>Sign Out</button> */}
+            <Navbar
+                handleChangeFilter={handleChangeFilter}
+            />
+            <div className='desktop-container'> 
+            <div className='search-div'>
+                <img className='search-icon' src={`https://lm-entertainment-app.netlify.app/static/media/icon-search.b3ef91bd.svg`} alt="" />
+                <input value={searchInput} onChange={(e)=>{setSearchInput(e.target.value)}} className='search-bar' type="text" placeholder={`Search for ${searchFor}`}/>
+            </div>
+            <p className='content-div-top'>Trending</p>
+            <div className='trending-div'>
+                {loadingContentData ? <Loader/> : contentData.map((c) => (
+                    c.attributes.trending ? (
+                        <ContentCard
+                            key={c.id}
+                            title={c.attributes.title}
+                            image={c.attributes.image.data.attributes.url}
+                            year={c.attributes.year}
+                            type={c.attributes.type}
+                        /> ) : null
+                ))}
+            </div>
+            <p className='content-div-top'>Recommended for you</p>
            <div className='contents-div'>
-                {contentData && contentData.map((c) => (
+                {loadingContentData ? <Loader/> : contentData.map((c) => (
+                 !c.attributes.trending ? (
                  <ContentCard
                     key={c.id}
                     title={c.attributes.title}
                     image={c.attributes.image.data.attributes.url}
-                 /> 
+                    year={c.attributes.year}
+                    type={c.attributes.type}
+                 /> ) : null
                 ))}
+            </div>
             </div>
         </div>
     );
 }
 
-export default Dashboard;
+export default Dashboard;  
